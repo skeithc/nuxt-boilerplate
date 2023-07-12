fada31b3de6729263c031fe55d7c217d40d1faec
Skeith <github@skeith.dev>
2023-07-12 14:12:06 +0800

    Add ESLint support and enforce the Airbnb standard

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+require("@rushstack/eslint-patch/modern-module-resolution")
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-airbnb-with-typescript'
+  ]
+}
