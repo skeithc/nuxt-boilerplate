cad29331cf1f11ee6d4e46034feb2ff37f384baa
Skeith <github@skeith.dev>
2023-07-12 15:28:26 +0800

    Fix linting issues in .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
-require("@rushstack/eslint-patch/modern-module-resolution")
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
   extends: [
     'plugin:vue/vue3-recommended',
-    '@vue/eslint-config-airbnb-with-typescript'
-  ]
-}
+    '@vue/eslint-config-airbnb-with-typescript',
+  ],
+};
