e00716345c990df14857ad797c85964fa1da6e9b
Skeith <github@skeith.dev>
2023-07-12 16:28:32 +0800

    Add VueUse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,9 @@ import { defineNuxtConfig } from 'nuxt/config';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/eslint-module',
+    '@vueuse/nuxt',
+  ],
 });
